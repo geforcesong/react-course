39166cf41e4f24d3214485b2b27317310534a180
geforcesong <georgeguo@outlook.com>
2022-06-13 16:11:44 +0800

    added some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      orange: colors.orange,
+    },
     extend: {},
   },
   plugins: [],
