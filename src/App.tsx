b5fb5a2df421a36e3fa1f6889935e1992d176315
geforcesong <georgeguo@outlook.com>
2022-06-14 11:20:09 +0800

    updated prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { NavBar } from "./components/common/NavBar";
+import { NavBar } from './components/common/NavBar';
 function App() {
-  return <div>
-    <NavBar />
-    <h1 className="text-orange-600 font-bold underline">Hello world!</h1>
+  return (
+    <div>
+      <NavBar />
+      <h1 className='text-orange-600 font-bold underline'>Hello world!</h1>
 
-    <i className="icon-close"></i>
-  </div>
+      <i className='icon-close'></i>
+    </div>
+  );
 }
 
 export default App;
--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,34 +1,34 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo } from 'react';
 
 export const NavBar: React.FC = () => {
   const [mobileMenuStatus, setMobileMenuStatus] = useState<boolean>(false);
   const menus = useMemo(
     () => [
       {
-        text: "Home",
-        link: "/",
+        text: 'Home',
+        link: '/',
       },
       {
-        text: "About",
+        text: 'About',
         subMenus: [
-          { text: "Demo", link: "/demo" },
-          { text: "Contact", link: "/contact" },
-          { text: "License", link: "/license" },
+          { text: 'Demo', link: '/demo' },
+          { text: 'Contact', link: '/contact' },
+          { text: 'License', link: '/license' },
         ],
       },
       {
-        text: "Demos",
+        text: 'Demos',
         subMenus: [
-          { text: "Countries", link: "/countries" },
-          { text: "Calculator", link: "/calculator" },
+          { text: 'Countries', link: '/countries' },
+          { text: 'Calculator', link: '/calculator' },
         ],
       },
       {
-        text: "Services",
-        link: "/services",
+        text: 'Services',
+        link: '/services',
       },
     ],
-    []
+    [],
   );
 
   const handleMobileClick = () => {
@@ -36,23 +36,23 @@ export const NavBar: React.FC = () => {
   };
 
   return (
-    <nav className="bg-white shadow-md font-semibold">
+    <nav className='bg-white shadow-md font-semibold'>
       <div>
-        <div className="flex justify-between bg-black">
-          <div className="flex">
+        <div className='flex justify-between bg-black'>
+          <div className='flex'>
             <div>
-              <a href="#" className="flex items-center py-4 px-2">
-                <span className="text-white text-lg">React Course</span>
+              <a href='#' className='flex items-center py-4 px-2'>
+                <span className='text-white text-lg'>React Course</span>
               </a>
             </div>
-            <div className="hidden md:flex items-center">
+            <div className='hidden md:flex items-center'>
               {menus.map((menu) => {
                 if (!menu.subMenus || !menu.subMenus.length) {
                   return (
                     <a
                       key={menu.text}
                       href={menu.link}
-                      className="py-4 px-2 text-gray-100 hover:text-white hover:bg-green-500"
+                      className='py-4 px-2 text-gray-100 hover:text-white hover:bg-green-500'
                     >
                       {menu.text}
                     </a>
@@ -61,17 +61,17 @@ export const NavBar: React.FC = () => {
                 return (
                   <div
                     key={menu.text}
-                    className="group relative py-4 px-2 text-gray-100 hover:text-white hover:bg-green-500"
+                    className='group relative py-4 px-2 text-gray-100 hover:text-white hover:bg-green-500'
                   >
                     <button>{menu.text}</button>
-                    <div className="hidden flex-col group-hover:flex text-white bg-black absolute py-1 px-1 top-[55px] left-[0px]">
+                    <div className='hidden flex-col group-hover:flex text-white bg-black absolute py-1 px-1 top-[55px] left-[0px]'>
                       {menu.subMenus.map((sm) => {
                         return (
                           <div
-                            className="py-1 px-2 hover:bg-green-500"
+                            className='py-1 px-2 hover:bg-green-500'
                             key={sm.text}
                           >
-                            <a href={sm.link} className="text-white ">
+                            <a href={sm.link} className='text-white '>
                               {sm.text}
                             </a>
                           </div>
@@ -83,28 +83,28 @@ export const NavBar: React.FC = () => {
               })}
             </div>
           </div>
-          <div className="hidden md:flex items-center space-x-3 ">
+          <div className='hidden md:flex items-center space-x-3 '>
             <a
-              href=""
-              className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-green-500 hover:text-white transition duration-300"
+              href=''
+              className='py-2 px-2 font-medium text-gray-500 rounded hover:bg-green-500 hover:text-white transition duration-300'
             >
               Log In
             </a>
             <a
-              href=""
-              className="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300"
+              href=''
+              className='py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300'
             >
               Sign Up
             </a>
           </div>
-          <div className="md:hidden flex items-center">
+          <div className='md:hidden flex items-center'>
             <button
-              className="outline-none mobile-menu-button"
+              className='outline-none mobile-menu-button'
               onClick={handleMobileClick}
             >
               <i
                 className={`${
-                  mobileMenuStatus ? "icon-close" : "icon-view-list"
+                  mobileMenuStatus ? 'icon-close' : 'icon-view-list'
                 } text-white text-xl mr-2`}
               ></i>
             </button>
@@ -112,13 +112,13 @@ export const NavBar: React.FC = () => {
         </div>
       </div>
       {mobileMenuStatus && (
-        <div className="mobile-menu">
-          <ul className="">
+        <div className='mobile-menu'>
+          <ul className=''>
             {menus.map((menu) => {
               return (
                 <li key={menu.text}>
                   <div>
-                    <a href={menu.link} className="block text-sm px-2 py-2">
+                    <a href={menu.link} className='block text-sm px-2 py-2'>
                       {menu.text}
                     </a>
                     {menu.subMenus?.length &&
@@ -127,7 +127,7 @@ export const NavBar: React.FC = () => {
                           <a
                             key={subMenu.text}
                             href={subMenu.link}
-                            className="block text-sm pl-8 py-2"
+                            className='block text-sm pl-8 py-2'
                           >
                             {subMenu.text}
                           </a>
