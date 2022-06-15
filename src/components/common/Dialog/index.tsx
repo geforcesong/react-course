191c86f98c14664ffa29db2a1f7443dc2f9582ba
geforcesong <georgeguo@outlook.com>
2022-06-15 08:30:14 +0800

    added dialog

---
+++ src/components/common/Dialog/index.tsx
@@ -0,0 +1,63 @@
+import React, { useRef, useImperativeHandle } from 'react';
+import styles from './index.module.scss';
+
+type DialogProps = {
+  onClosed?: () => void;
+  children: React.ReactNode;
+};
+
+type DialogHandles = {
+  show: () => void;
+  showModal: () => void;
+  closeModal: () => void;
+};
+
+const Dialog = React.forwardRef<DialogHandles, DialogProps>((props, ref) => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  const closeModal = () => {
+    dialogRef.current!.setAttribute('closing', '');
+    dialogRef.current!.addEventListener(
+      'animationend',
+      () => {
+        dialogRef.current!.removeAttribute('closing');
+        props?.onClosed?.();
+        dialogRef.current!.close();
+      },
+      { once: true },
+    );
+  };
+
+  const showModal = () => {
+    dialogRef.current!.showModal();
+  };
+  const show = () => {
+    dialogRef.current!.show();
+  };
+
+  useImperativeHandle(ref, () => ({
+    show,
+    showModal,
+    closeModal,
+  }));
+
+  const handleClick = (e: React.MouseEvent<HTMLDialogElement, MouseEvent>) => {
+    const target = e.target as HTMLElement;
+    if (target?.nodeName === 'DIALOG') {
+      closeModal();
+    }
+  };
+
+  return (
+    <dialog
+      className={styles['my-dialog']}
+      ref={dialogRef}
+      onClick={handleClick}
+    >
+      <div>{props.children}</div>
+    </dialog>
+  );
+});
+Dialog.displayName = 'Dialog';
+
+export default Dialog;
--- src/pages/LicensePage.tsx
+++ src/pages/LicensePage.tsx
@@ -1,4 +1,32 @@
+import { useRef } from 'react';
+import Dialog from '../components/common/Dialog';
+
+type DialogHandle = React.ElementRef<typeof Dialog>;
+
 const LicensePage: React.FC = () => {
-  return <div>License Page</div>;
+  const dialogRef = useRef<DialogHandle>(null);
+
+  const handleClick = () => {
+    if (dialogRef) {
+      dialogRef.current!.showModal();
+    }
+  };
+
+  const handleClose = () => {
+    dialogRef?.current!.closeModal();
+  };
+
+  return (
+    <div>
+      License Page
+      <button onClick={handleClick}>Click Me</button>
+      <Dialog ref={dialogRef}>
+        <div>
+          <h2>Haha, I am show up!</h2>
+          <button onClick={handleClose}>Close Me</button>
+        </div>
+      </Dialog>
+    </div>
+  );
 };
 export default LicensePage;
