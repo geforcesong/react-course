6c2eb7f7e5b0d77baa4430cb99802a03977a4aec
geforcesong <georgeguo@outlook.com>
2022-06-23 11:48:14 +0800

    updated

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,4 +1,62 @@
 const AboutPage: React.FC = () => {
-  return <div>About Page</div>;
+  return (
+    <div>
+      <div className='p-6 font-medium'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        mollitia ex sint consequuntur, repudiandae minima, illum numquam quae
+        similique soluta expedita eius incidunt illo perferendis assumenda
+        laborum necessitatibus? Est, ea.
+      </div>
+      <div className='p-6 font-medium'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        mollitia ex sint consequuntur, repudiandae minima, illum numquam quae
+        similique soluta expedita eius incidunt illo perferendis assumenda
+        laborum necessitatibus? Est, ea.
+      </div>
+      <div className='p-6 font-medium'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        mollitia ex sint consequuntur, repudiandae minima, illum numquam quae
+        similique soluta expedita eius incidunt illo perferendis assumenda
+        laborum necessitatibus? Est, ea.
+      </div>
+      <div className='p-6 font-medium'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        mollitia ex sint consequuntur, repudiandae minima, illum numquam quae
+        similique soluta expedita eius incidunt illo perferendis assumenda
+        laborum necessitatibus? Est, ea.
+      </div>
+      <div className='flex p-4'>
+        <a href='#item1' className='text-blue-900 p-4'>
+          Item1
+        </a>
+        <a href='#item2' className='text-blue-900 p-4'>
+          Item2
+        </a>
+        <a href='#item3' className='text-blue-900 p-4'>
+          Item3
+        </a>
+      </div>
+      <div className='p-6 font-medium'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        mollitia ex sint consequuntur, repudiandae minima, illum numquam quae
+        similique soluta expedita eius incidunt illo perferendis assumenda
+        laborum necessitatibus? Est, ea.
+      </div>
+      <div className='w-48 h-48 rounded-full bg-blue-700 text-white flex items-center justify-center'>
+        Hello world
+      </div>
+      <div className='flex'>
+        <div id='item1' className='p-8 m-4 flex border-2'>
+          Item 1
+        </div>
+        <div id='item2' className='p-8 m-4 flex border-2'>
+          Item 2
+        </div>
+        <div id='item3' className='p-8 m-4 flex border-2'>
+          Item 3
+        </div>
+      </div>
+    </div>
+  );
 };
 export default AboutPage;
